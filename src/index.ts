4eb81f93c48936714ac83bea2afb0b4d06b6b1bd
stephen-nene <stevekid705@mail.com>
2023-12-27 13:56:12 +0300

    new functions

--- src/index.ts
+++ src/index.ts
@@ -20,51 +20,95 @@ const debtStorage = new StableBTreeMap<string, DebtRecord>(0, 44, 1024);
 
 $query;
 export function getDebts(): Result<Vec<DebtRecord>, string> {
-    return Result.Ok(debtStorage.values());
+    try {
+        return Result.Ok(debtStorage.values());
+    } catch (error) {
+        return Result.Err<DebtRecord[], string>(`Error retrieving debts: ${error}`);
+    }
+}
+
+$query;
+export function getTotalDebt(): Result<number, string> {
+    try {
+        const totalDebt = debtStorage.values().reduce((acc, debt) => acc + debt.amount, 0);
+        return Result.Ok(totalDebt);
+    } catch (error) {
+        return Result.Err<number, string>(`Error calculating total debt: ${error}`);
+    }
+}
+
+$query;
+export function searchDebts(query: string): Result<Vec<DebtRecord>, string> {
+    try {
+        const searchResults = debtStorage.values().filter(
+            (debt) =>
+                debt.description.toLowerCase().includes(query.toLowerCase()) ||
+                debt.debtorName.toLowerCase().includes(query.toLowerCase())
+        );
+        return Result.Ok(searchResults);
+    } catch (error) {
+        return Result.Err<Vec<DebtRecord>, string>(`Error searching debts: ${error}`);
+    }
 }
 
 $query;
 export function getDebt(id: string): Result<DebtRecord, string> {
-    return match(debtStorage.get(id), {
-        Some: (debt) => Result.Ok<DebtRecord, string>(debt),
-        None: () => Result.Err<DebtRecord, string>(`Debt with id=${id} not found`)
-    });
+    try {
+        return match(debtStorage.get(id), {
+            Some: (debt) => Result.Ok<DebtRecord, string>(debt),
+            None: () => Result.Err<DebtRecord, string>(`Debt with id=${id} not found`)
+        });
+    } catch (error) {
+        return Result.Err<DebtRecord, string>(`Error retrieving debt: ${error}`);
+    }
 }
 
 $update;
 export function addDebt(payload: DebtPayload): Result<DebtRecord, string> {
-    const debt: DebtRecord = {
-        id: uuidv4(),
-        createdAt: ic.time(),
-        updatedAt: Opt.None,
-        ...payload
-    };
-    debtStorage.insert(debt.id, debt);
-    return Result.Ok(debt);
+    try {
+        const debt: DebtRecord = {
+            id: uuidv4(),
+            createdAt: ic.time(),
+            updatedAt: Opt.None,
+            ...payload
+        };
+        debtStorage.insert(debt.id, debt);
+        return Result.Ok(debt);
+    } catch (error) {
+        return Result.Err<DebtRecord, string>(`Error adding debt: ${error}`);
+    }
 }
 
 $update;
 export function updateDebt(id: string, payload: DebtPayload): Result<DebtRecord, string> {
-    return match(debtStorage.get(id), {
-        Some: (debt) => {
-            const updatedDebt: DebtRecord = {
-                ...debt,
-                ...payload,
-                updatedAt: Opt.Some(ic.time())
-            };
-            debtStorage.insert(debt.id, updatedDebt);
-            return Result.Ok<DebtRecord, string>(updatedDebt);
-        },
-        None: () => Result.Err<DebtRecord, string>(`Debt with id=${id} not found`)
-    });
+    try {
+        return match(debtStorage.get(id), {
+            Some: (debt) => {
+                const updatedDebt: DebtRecord = {
+                    ...debt,
+                    ...payload,
+                    updatedAt: Opt.Some(ic.time())
+                };
+                debtStorage.insert(debt.id, updatedDebt);
+                return Result.Ok<DebtRecord, string>(updatedDebt);
+            },
+            None: () => Result.Err<DebtRecord, string>(`Debt with id=${id} not found`)
+        });
+    } catch (error) {
+        return Result.Err<DebtRecord, string>(`Error updating debt: ${error}`);
+    }
 }
 
 $update;
 export function deleteDebt(id: string): Result<DebtRecord, string> {
-    return match(debtStorage.remove(id), {
-        Some: (deletedDebt) => Result.Ok<DebtRecord, string>(deletedDebt),
-        None: () => Result.Err<DebtRecord, string>(`Debt with id=${id} not found.`)
-    });
+    try {
+        return match(debtStorage.remove(id), {
+            Some: (deletedDebt) => Result.Ok<DebtRecord, string>(deletedDebt),
+            None: () => Result.Err<DebtRecord, string>(`Debt with id=${id} not found.`)
+        });
+    } catch (error) {
+        return Result.Err<DebtRecord, string>(`Error deleting debt: ${error}`);
+    }
 }
 
 // a workaround to make uuid package work with Azle
